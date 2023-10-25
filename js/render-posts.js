8a5493c435a398a24e9779d3c915daacdaae46cb
heligie <heliemzh@gmail.com>
2023-10-25 21:59:36 +0300

    fix: change function

--- js/render-posts.js
+++ js/render-posts.js
@@ -5,16 +5,18 @@ const container = document.querySelector('.pictures');
 const template = document.querySelector('#picture').content.querySelector('.picture');
 const fragment = document.createDocumentFragment();
 
+const createPhoto = (post) => {
+  const newPhoto = template.cloneNode(true);
+  const img = newPhoto.querySelector('.picture__img');
+  img.src = post.url;
+  img.alt = post.description;
+  newPhoto.querySelector('.picture__likes').textContent = post.likes;
+  newPhoto.querySelector('.picture__comments').textContent = post.comments.length;
+  fragment.append(newPhoto);
+};
+
 const renderPosts = () => {
-  posts.forEach((post) => {
-    const newPhoto = template.cloneNode(true);
-    const img = newPhoto.querySelector('.picture__img');
-    img.src = post.url;
-    img.alt = post.description;
-    newPhoto.querySelector('.picture__likes').textContent = post.likes;
-    newPhoto.querySelector('.picture__comments').textContent = post.comments.length;
-    fragment.append(newPhoto);
-  });
+  posts.forEach((post) => createPhoto(post));
   container.append(fragment);
 };
 
