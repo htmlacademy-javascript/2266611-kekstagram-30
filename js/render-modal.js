aed17014348663a142b15d5d447cb2487814bea7
heligie <heliemzh@gmail.com>
2023-10-31 23:26:27 +0300

    feat: add updating the comments counter and uploading comments

--- js/render-modal.js
+++ js/render-modal.js
@@ -1,20 +1,62 @@
 import {isEscapeKey} from './utils.js';
 
+const COMMENTS_INTERVAL = 5;
+let visibleComments = 0;
+
 const modalContainer = document.querySelector('.big-picture');
 const modalCloseButton = document.querySelector('.big-picture__cancel');
 const fullSizePhoto = document.querySelector('.big-picture__img img');
 const postCaption = document.querySelector('.social__caption');
-const postLikesCount = document.querySelector('.likes-count');
+const postLikesCounter = document.querySelector('.likes-count');
 const commentsList = document.querySelector('.social__comments');
 const commentsItem = document.querySelector('.social__comment');
+const commentsShownCounter = document.querySelector('.social__comment-shown-count');
+const commentsTotalCounter = document.querySelector('.social__comment-total-count');
+const commentsLoadingButton = document.querySelector('.comments-loader');
 
 let comments = [];
 
+const updateCommentsCounter = () => {
+  visibleComments += COMMENTS_INTERVAL;
+  if (visibleComments > comments.length) {
+    visibleComments = comments.length;
+  }
+  commentsShownCounter.textContent = visibleComments;
+  commentsTotalCounter.textContent = comments.length;
+};
+
+const hideCommentsLoadingButton = () => {
+  if (visibleComments >= comments.length) {
+    commentsLoadingButton.classList.add('hidden');
+    return;
+  }
+  commentsLoadingButton.classList.remove('hidden');
+};
+
+const createComment = (comment) => {
+  const newComment = commentsItem.cloneNode(true);
+  const img = newComment.querySelector('.social__picture');
+  img.src = comment.avatar;
+  img.alt = comment.name;
+  newComment.querySelector('.social__text').textContent = comment.message;
+  return newComment;
+};
+
+const fillComments = () => {
+  const currentComments = comments.slice(visibleComments, visibleComments + COMMENTS_INTERVAL);
+  currentComments.forEach((comment) => commentsList.append(createComment(comment)));
+  updateCommentsCounter();
+  hideCommentsLoadingButton();
+};
+
 const openModal = () => {
+  commentsList.innerHTML = '';
+  visibleComments = 0;
   modalContainer.classList.remove('hidden');
   document.body.classList.add('modal-open');
   modalCloseButton.addEventListener('click', modalCloseButtonClickHandler);
   document.addEventListener('keydown', documentKeydownHandler);
+  commentsLoadingButton.addEventListener('click', commentsLoadingButtonClickHandler);
 };
 
 const closeModal = () => {
@@ -22,6 +64,7 @@ const closeModal = () => {
   document.body.classList.remove('modal-open');
   modalCloseButton.removeEventListener('click', modalCloseButtonClickHandler);
   document.removeEventListener('keydown', documentKeydownHandler);
+  commentsLoadingButton.removeEventListener('click', commentsLoadingButtonClickHandler);
 };
 
 function modalCloseButtonClickHandler() {
@@ -35,28 +78,18 @@ function documentKeydownHandler(evt) {
   }
 }
 
+function commentsLoadingButtonClickHandler() {
+  fillComments();
+}
+
 const fillPostInfo = (post) => {
   fullSizePhoto.src = post.url;
   fullSizePhoto.alt = post.description;
   postCaption.textContent = post.description;
-  postLikesCount.textContent = post.likes;
-};
-
-const createComment = (comment) => {
-  const newComment = commentsItem.cloneNode(true);
-  const img = newComment.querySelector('.social__picture');
-  img.src = comment.avatar;
-  img.alt = comment.name;
-  newComment.querySelector('.social__text').textContent = comment.message;
-  return newComment;
-};
-
-const fillComments = () => {
-  comments.forEach((comment) => commentsList.append(createComment(comment)));
+  postLikesCounter.textContent = post.likes;
 };
 
 const renderModal = (post) => {
-  commentsList.innerHTML = '';
   comments = post.comments;
   openModal();
   fillPostInfo(post);
